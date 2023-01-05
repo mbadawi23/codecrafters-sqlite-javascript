66cdcbeef45bb688abaf073066ab1d1f6796c496
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2023-01-04 21:46:03 -0800

    STAGE #3: Implementing `.tables`.

--- app/main.js
+++ app/main.js
@@ -1,9 +1,11 @@
 import { open } from "fs/promises";
 
+const COMMANDS = [".dbinfo", ".tables"];
+
 const databaseFilePath = process.argv[2];
 const command = process.argv[3];
 
-if (command === ".dbinfo") {
+if (COMMANDS.includes(command)) {
   const databaseFileHandler = await open(databaseFilePath, "r");
 
   const { buffer: fileHeaderBuffer } = await databaseFileHandler.read({
@@ -17,7 +19,6 @@ if (command === ".dbinfo") {
 
   // Uncomment this to pass the first stage
   const pageSize = fileHeaderBuffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
-  console.log(`database page size: ${pageSize}`);
 
   const { buffer: pageHeaderBuffer } = await databaseFileHandler.read({
     length: pageSize,
@@ -25,11 +26,18 @@ if (command === ".dbinfo") {
     buffer: Buffer.alloc(pageSize),
   });
 
-  // console.log("pageHeaderBuffer", pageHeaderBuffer.toString());
+  const tables = (
+    pageHeaderBuffer.toString().match(/CREATE TABLE\s{1}\w+\(*\w*,?\w*\)*/g) ||
+    []
+  ).map((element) => element.replace("CREATE TABLE", "").trim());
 
-  const tableCount = (pageHeaderBuffer.toString().match(/CREATE TABLE/g) || [])
-    .length;
-  console.log("number of tables: ", tableCount);
+  if (command === ".dbinfo") {
+    console.log(`database page size: ${pageSize}`);
+    console.log("number of tables: ", tables.length);
+  }
+  if (command === ".tables") {
+    console.log("tables:", ...tables.reverse());
+  }
 } else {
   throw `Unknown command ${command}`;
 }
